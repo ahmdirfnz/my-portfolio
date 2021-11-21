50c0d4255001e8db76b4163b365ce6e7cfa7898c
irfanZ <46247388+irfanZ1357@users.noreply.github.com>
2021-11-21 21:37:02 +0800

    First Commit

---
+++ src/App.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="app" class="bg-img">
+    <Navbar />
+    <!-- <Home msg="Hi I am irfanz"/> -->
+  </div>
+</template>
+
+<script>
+// import Home from './components/Home.vue'
+import Navbar from './components/Navbar.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Navbar
+    // Home    
+  }
+}
+</script>
+
+<style>
+
+body, html {
+  height: 100%;
+  padding-top: 0px !important;
+}
+
+#app {
+  margin: 0px;
+  height: 100%;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+.bg-img {
+  background-image: url("./assets/sandWallpaper.jpg");
+  background-position: center;
+  background-repeat:  no-repeat;
+  background-size:  cover;
+  height: 100%;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+<b-navbar :fixed-top="true" :transparent="false" type="is-dark">
+  <template #brand>
+    <b-navbar-item tag="router-link" :to="{ path: '/'}">
+      <img
+        src="../assets/terminal.jpg"
+        alt="Itachi Uchiha"
+      >
+    </b-navbar-item>
+  </template> 
+  <template #start>
+    <b-navbar-item href="#">
+      Home
+    </b-navbar-item>
+    <b-navbar-item href="#">
+      Skills
+    </b-navbar-item>
+    <b-navbar-item href="#">
+      About
+    </b-navbar-item>
+  </template>
+  <template #end>
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <strong>Subscribe</strong>
+                    </a>
+                    <a class="button is-light">
+                        Say Hi
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+</b-navbar>
+</template>
+
+<script>
+export default {
+    name: 'Navbar'
+}
+</script>
+
+<style>
+.nav {
+    font-size: 20px;
+}
+
+nav.navbar.is-fixed-top {
+  background: transparent;
+}
+</style>
